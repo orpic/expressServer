4dfc4dcd021ced4866c19e4dbcb91d787ecd0bf2
shobhit <shobhitumrao5@gmail.com>
2023-07-18 21:38:51 +0530

    towards mvc architecture

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,15 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   console.log("add product");
-  res.send(
-    '<form action="/product" method="POST" ><input type="text" name="title" ><button type="submit">add product</button></form>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });

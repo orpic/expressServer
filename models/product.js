da97bf5095eafb1dca975404eb3c5b632e219644
shobhit <shobhitumrao5@gmail.com>
2023-08-28 22:05:58 +0530

    Auto-commit, message:- saving to file works
      A  data/products.json
      M  models/product.js

--- models/product.js
+++ models/product.js
@@ -1,4 +1,7 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const pathDir = require("./../path");
 
 module.exports = class Product {
   constructor(t) {
@@ -6,10 +9,21 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    console.log(pathDir);
+    const p = path.join(pathDir, "data", "products.json");
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    return [];
   }
 };

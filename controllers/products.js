9e12ce00adb440e5c5b0e432646807944624ea99
shobhit <shobhitumrao5@gmail.com>
2023-08-28 23:07:47 +0530

    Auto-commit, message:- mvc conversion complete
      M  controllers/products.js
      M  data/products.json
      M  models/product.js

--- controllers/products.js
+++ controllers/products.js
@@ -22,13 +22,14 @@ exports.postAddProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
   //   const products = products;
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
-  const products = Product.fetchAll();
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCss: true,
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCss: true,
+    });
   });
 };

343df98d1819d523085695b4d9e0c1a7b808f048
shobhit <shobhitumrao5@gmail.com>
2023-08-28 21:01:54 +0530

    Auto-commit, message:- models added
      M  controllers/products.js
      A  models/product.js

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require("./../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   console.log("add product");
@@ -14,13 +14,15 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   console.log(req.body);
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
   //   const products = products;
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = Product.fetchAll();
   res.render("shop", {
     prods: products,
     pageTitle: "Shop",
